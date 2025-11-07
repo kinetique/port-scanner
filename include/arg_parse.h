f06dac83020794e23329e3c9769b62844ed0ea9b
kinetique <sonyacat0423@gmail.com>
2025-11-07 12:07:12 +0200

    added macroses and public functions

--- include/arg_parse.h
+++ include/arg_parse.h
@@ -45,4 +45,45 @@ struct argparse {
     const char *optvalue;
 };
 
+#define OPT_END() \
+    { 0, NULL, NULL, ARGPARSE_OPT_END, 0, NULL, 0, NULL }
+
+#define OPT_BOOLEAN(s,l,ptr,help_text) \
+    { (s), (l), (void *)(ptr), ARGPARSE_OPT_BOOLEAN, 0, (help_text), 0, NULL }
+
+#define OPT_BIT(s,l,ptr,bit,help_text) \
+    { (s), (l), (void *)(ptr), ARGPARSE_OPT_BIT, (bit), (help_text), 0, NULL }
+
+#define OPT_STRING(s,l,ptr,help_text) \
+    { (s), (l), (void *)(ptr), ARGPARSE_OPT_STRING, 0, (help_text), 0, NULL }
+
+#define OPT_INTEGER(s,l,ptr,help_text) \
+    { (s), (l), (void *)(ptr), ARGPARSE_OPT_INTEGER, 0, (help_text), 0, NULL }
+
+#define OPT_FLOAT(s,l,ptr,help_text) \
+    { (s), (l), (void *)(ptr), ARGPARSE_OPT_FLOAT, 0, (help_text), 0, NULL }
+
+#define OPT_GROUP(help_text) \
+    { 0, NULL, NULL, ARGPARSE_OPT_GROUP, 0, (help_text), 0, NULL }
+
+#define OPT_CUSTOM(s,l,ptr,type_,data_,help_,flags_,cb_) \
+    { (s), (l), (void *)(ptr), (type_), (data_), (help_), (flags_), (cb_) }
+
+    
+int argparse_init(struct argparse *self, struct argparse_option *options,
+                  const char *const *usages, int flags);
+
+void argparse_describe(struct argparse *self, const char *description,
+                       const char *epilog);
+
+int argparse_parse(struct argparse *self, int argc, const char **argv);
+
+void argparse_usage(struct argparse *self);
+
+int argparse_help_cb_no_exit(struct argparse *self,
+                             const struct argparse_option *option);
+
+int argparse_help_cb(struct argparse *self,
+                     const struct argparse_option *option);
+
 #endif // ARG_PARSE_H
