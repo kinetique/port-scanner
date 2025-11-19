717ccfa2b3843ebad2df24a584c6693b29857faf
kinetique <sonyacat0423@gmail.com>
2025-11-19 16:08:48 +0200

    fixed some bugs and added manual testing

--- include/arg_parse.h
+++ include/arg_parse.h
@@ -3,31 +3,34 @@
 
 #include <stddef.h>
 
-#define ARGPARSE_OPT_BOOLEAN 0
-#define ARGPARSE_OPT_BIT 1
-#define ARGPARSE_OPT_STRING 2
-#define ARGPARSE_OPT_INT 3
-#define ARGPARSE_OPT_GROUP 4
-#define ARGPARSE_OPT_END 5
+#define ARGPARSE_OPT_BOOLEAN  0
+#define ARGPARSE_OPT_BIT      1
+#define ARGPARSE_OPT_STRING   2
+#define ARGPARSE_OPT_INTEGER  3   
+#define ARGPARSE_OPT_GROUP    4
+#define ARGPARSE_OPT_END      5
 
-#define ARGPARSE_OPT_NONEG (1<<0)
+#define ARGPARSE_OPT_NONEG (1 << 0)
 
-#define ARGPARSE_STOP_AT_NONOPTION (1<<0)
+#define ARGPARSE_STOP_AT_NONOPTION (1 << 0)
+#define ARGPARSE_IGNORE_UNKNOWN_ARGS (1 << 1)
 
-struct argparse_option;
 struct argparse;
+struct argparse_option;
 
 typedef void (*argparse_callback_t)(struct argparse *self,
-                                     const struct argparse_option *option);
+                                    const struct argparse_option *option);
+
 
 struct argparse_option {
-    char short_name;
-    const char *long_name;
-    int type;
-    void *value;
-    const char *help;
-    int flags;
-    argparse_callback_t callback;
+    char short_name;                
+    const char *long_name;          
+    void *value;                    
+    int type;                       
+    int data;                       
+    const char *help;               
+    int flags;                      
+    argparse_callback_t callback;   
 };
 
 struct argparse {
@@ -36,16 +39,13 @@ struct argparse {
     int flags;
     const char *description;
     const char *epilog;
-    
+
     int argc;
     const char **argv;
     const char **out;
     int cpidx;
     const char *optvalue;
 };
-
-#define OPT_END() \
-    { 0, NULL, NULL, ARGPARSE_OPT_END, 0, NULL, 0, NULL }
 
 #define OPT_BOOLEAN(s,l,ptr,help_text) \
     { (s), (l), (void *)(ptr), ARGPARSE_OPT_BOOLEAN, 0, (help_text), 0, NULL }
@@ -59,15 +59,14 @@ struct argparse {
 #define OPT_INTEGER(s,l,ptr,help_text) \
     { (s), (l), (void *)(ptr), ARGPARSE_OPT_INTEGER, 0, (help_text), 0, NULL }
 
-#define OPT_FLOAT(s,l,ptr,help_text) \
-    { (s), (l), (void *)(ptr), ARGPARSE_OPT_FLOAT, 0, (help_text), 0, NULL }
-
 #define OPT_GROUP(help_text) \
     { 0, NULL, NULL, ARGPARSE_OPT_GROUP, 0, (help_text), 0, NULL }
 
 #define OPT_CUSTOM(s,l,ptr,type_,data_,help_,flags_,cb_) \
     { (s), (l), (void *)(ptr), (type_), (data_), (help_), (flags_), (cb_) }
 
+#define OPT_END() \
+    { 0, NULL, NULL, ARGPARSE_OPT_END, 0, NULL, 0, NULL }
 
 int argparse_init(struct argparse *self, struct argparse_option *options,
                   const char *const *usages, int flags);
@@ -85,4 +84,4 @@ int argparse_help_cb_no_exit(struct argparse *self,
 int argparse_help_cb(struct argparse *self,
                      const struct argparse_option *option);
 
-#endif // ARG_PARSE_H
+#endif // ARG_PARSE_H 
--- src/arg_parse.c
+++ src/arg_parse.c
@@ -95,3 +95,142 @@ void argparse_describe(struct argparse *self,
     self->description = description;
     self->epilog = epilog;
 }
+
+int argparse_parse(struct argparse *self, int argc, const char **argv)
+{
+    self->argc = argc;
+    self->argv = argv;
+
+    while (self->argc > 0) {
+        const char *arg = *self->argv;
+
+        /* Non-option argument */
+        if (arg[0] != '-') {
+            if (self->flags & ARGPARSE_STOP_AT_NONOPTION)
+                break;
+            self->out[self->cpidx++] = arg;
+            self->argv++; self->argc--;
+            continue;
+        }
+
+        /* "--" indicates the end of option parsing */
+        if (strcmp(arg, "--") == 0) {
+            self->argv++; self->argc--;
+            break;
+        }
+
+        /* Long option */
+        if (arg[1] == '-') {
+            const char *name = arg + 2;
+            const char *value = NULL;
+
+            char buf[256];
+            const char *eq = strchr(name, '=');
+            if (eq) {
+                size_t len = eq - name;
+                memcpy(buf, name, len);
+                buf[len] = '\0';
+                name = buf;
+                value = eq + 1;
+            }
+
+            struct argparse_option *opt = find_option(self, 0, name);
+            if (!opt) {
+                if (!(self->flags & ARGPARSE_IGNORE_UNKNOWN_ARGS)) {
+                    printf("Unknown option: %s\n", arg);
+                    exit(1);
+                }
+                self->argv++; self->argc--;
+                continue;
+            }
+
+            /* Boolean option */
+            if (opt->type == ARGPARSE_OPT_BOOLEAN) {
+                *(int *)opt->value = 1;
+            }
+            /* Integer */
+            else if (opt->type == ARGPARSE_OPT_INTEGER) {
+                if (!value) {
+                    if (self->argc < 2) {
+                        printf("Option --%s requires value\n", name);
+                        exit(1);
+                    }
+                    value = self->argv[1];
+                    self->argv++; self->argc--;
+                }
+                if (parse_int(value, (int *)opt->value) != 0) {
+                    printf("Invalid integer for --%s\n", name);
+                    exit(1);
+                }
+            }
+            /* String */
+            else if (opt->type == ARGPARSE_OPT_STRING) {
+                if (!value) {
+                    if (self->argc < 2) {
+                        printf("Option --%s requires value\n", name);
+                        exit(1);
+                    }
+                    value = self->argv[1];
+                    self->argv++; self->argc--;
+                }
+                *(const char **)opt->value = value;
+            }
+
+            if (opt->callback)
+                opt->callback(self, opt);
+
+            self->argv++; self->argc--;
+            continue;
+        }
+
+        /* Short option */
+        char short_name = arg[1];
+        struct argparse_option *opt = find_option(self, short_name, NULL);
+        if (!opt) {
+            printf("Unknown option: -%c\n", short_name);
+            exit(1);
+        }
+
+        const char *value = NULL;
+
+        if (opt->type == ARGPARSE_OPT_BOOLEAN) {
+            *(int *)opt->value = 1;
+        }
+        else if (opt->type == ARGPARSE_OPT_INTEGER) {
+            if (self->argc < 2) {
+                printf("Option -%c requires value\n", short_name);
+                exit(1);
+            }
+            value = self->argv[1];
+
+            if (parse_int(value, (int *)opt->value) != 0) {
+                printf("Invalid integer for -%c\n", short_name);
+                exit(1);
+            }
+            self->argv++; self->argc--;
+        }
+        else if (opt->type == ARGPARSE_OPT_STRING) {
+            if (self->argc < 2) {
+                printf("Option -%c requires value\n", short_name);
+                exit(1);
+            }
+            value = self->argv[1];
+            *(const char **)opt->value = value;
+            self->argv++; self->argc--;
+        }
+
+        if (opt->callback)
+            opt->callback(self, opt);
+
+        self->argv++; self->argc--;
+    }
+
+    /* Copy remaining non-option args */
+    while (self->argc > 0) {
+        self->out[self->cpidx++] = *self->argv;
+        self->argv++; self->argc--;
+    }
+
+    return self->cpidx;
+}
+
